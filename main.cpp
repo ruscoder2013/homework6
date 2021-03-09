f189c8d3ed84922b680477701d21f24ded7dbef3
rusdeveloper23 <gusev-a@mail.ru>
2021-03-09 22:33:02 +0300

    add

--- main.cpp
+++ main.cpp
@@ -2,20 +2,41 @@
 #include <string>
 #include <vector>
 #include <chrono>
+#include <sstream>
+#include <iomanip>
+#include <fstream>
 
-void print_bulk(std::vector<std::string>& commands, int& count)
-{
-    if (commands.size()==0) return;
-    std::cout << "bulk: ";
+std::string get_bulk_str(std::vector<std::string>& commands) {
+    std::ostringstream oss;
+    if (commands.size()==0) return "";
+    oss << "bulk: ";
     for(int i = 0; i < commands.size(); i++)
     {
         if (i>0)
-            std::cout << ", ";
-        std::cout << commands[i];
+            oss << ", ";
+        oss << commands[i];
     }
-    std::cout << std::endl;
+    oss << std::endl;
     commands.clear();
-    count = 0;
+    return oss.str();
+}
+
+std::string time_from_epoch()
+{
+    std::ostringstream oss;
+    using namespace std::chrono;
+    system_clock::time_point tp = system_clock::now();
+    system_clock::duration dtn = tp.time_since_epoch();
+    auto tt = dtn.count() * system_clock::period::num / system_clock::period::den;
+    oss << "bulk" << tt << ".log";
+    return oss.str();
+}
+
+void write_to_file(std::string file_name, std::string input) {
+    if (input.empty()) return;
+    std::ofstream out(file_name);
+    out << input;
+    out.close();
 }
 
 int main() {
@@ -23,49 +44,46 @@ int main() {
     std::cout << "N = ";
     std::cin >> N;
     std::vector<std::string> commands;
-    
-    std::string cmd; 
-    std::cin >> cmd;
-    commands.push_back(cmd);
-    int count = 1;
     std::chrono::milliseconds ms;
-    while(cmd.compare("end")!=0) {
-        if (count == N) {
-            print_bulk(commands, count);
-        }
-        std::cin >> cmd;
-        if(cmd.compare("{")==0)
-        {
-            print_bulk(commands, count); 
-            int brace_count = 1;
-            do {
-                std::cin >> cmd;
-                if(cmd.compare("{")==0)
-                {
-                    brace_count++;
-                }    
-                else if(cmd.compare("}")==0) {
-                    brace_count--;
-                } 
-                else {
-                    commands.push_back(cmd);
-                }
-            } while(brace_count!=0);
-            print_bulk(commands, count);
-        }
-        else if (cmd.compare("}")==0){
-            std::cout << "Error brace" << std::endl;
-            return -1;
+    std::string cmd;
+    std::string file_name; 
+    int brace_count = 0;
+    
+    std::cin >> cmd;
+    while(!std::cin.eof()) {
+        if(cmd.compare("{")==0) {
+            if(brace_count==0) {
+                auto str = get_bulk_str(commands);
+                std::cout << str;
+                write_to_file(file_name, str);
+            }
+            brace_count++;
+        } else if(cmd.compare("}")==0) {
+            brace_count--;
+            if(brace_count==0) {
+                auto str = get_bulk_str(commands);
+                std::cout << str;
+                write_to_file(file_name, str);
+            }
+            if(brace_count<0) {
+                std::cout << "Error brace" << std::endl;
+                return -1;
+            }
         } else {
             commands.push_back(cmd);
-            /*std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
-            std::time_t now_c = std::chrono::system_clock::to_time_t(now);
-            char buff [80]; 
-            strftime(buff, 20, "%d/%m/%Y:%H:%M:%S", localtime(&t));
-            string s(buff);
-            std::tm now_tm = *std::localtime(&now_c);*/
-            count++;
+            file_name = time_from_epoch();
         }
+        if((commands.size()>=N)&&(brace_count==0)) {
+            auto str = get_bulk_str(commands);
+            std::cout << str;
+            write_to_file(file_name, str);
+        }
+        std::cin >> cmd;
+    }
+    if(brace_count==0) {
+        auto str = get_bulk_str(commands);
+        std::cout << str;
+        write_to_file(file_name, str);
     }
     return 0;
 }
